d644a4fcead2b2ded908574aed2ba92620b596cc
Tim Tregubov <tim@zingweb.com>
2019-04-06 22:48:21 -0400

    updated to webpack 4 and fully working for 19s

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('starting up!');
+// const $ = require('jquery');
+//
+// $('#main').html('Here we go! Wheee');
+//
+
+// // change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+//
+// // this is a generator function.
+// // note the *
+function* simpleCounter() {
+  let count = 0;
+  while (true) {
+    yield count += 1;
+  }
+}
+
+// instantiate generator
+const counter = simpleCounter();
+
+setInterval(
+  () => { $('#main').html(`You have been on this page for ${counter.next().value} seconds.`); },
+  1000,
+);
